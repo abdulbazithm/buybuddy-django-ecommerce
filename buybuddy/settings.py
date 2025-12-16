700fc5b64873f76a5f1540c18b8ff88506a1433e
abdulbazithm <abdulbazithm001@gmail.com>
2025-12-16 14:29:00 +0530

    Fix 400 error: add CSRF_TRUSTED_ORIGINS

--- buybuddy/settings.py
+++ buybuddy/settings.py
@@ -32,6 +32,10 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
+CSRF_TRUSTED_ORIGINS = [
+    "https://buybuddy-wtzh.onrender.com",
+]
+
 
 
 
